f0527db9022c3cf7001f1c3dccee47f740b47e5c
lei334453 <1834877626@qq.com>
2022-05-23 20:52:48 +0800

    fix:0012

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+/*
+ * @edu-class-demo: personal
+ */
+/*
+ * @edu-class-demo: personal
+ */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  
   return (
-
-
-
-    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+
+
+
+
+
+          
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
